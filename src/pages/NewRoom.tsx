2ad6e703daf6a253b7feb8ecf7a8844248800644
v <matheusyanmonteiro@gmail.com>
2021-06-23 00:25:52 -0300

    created new room page

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,37 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie Salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as Dúvidas da sua audiência em tempo-real</p>
+      </aside>
+
+      <main>
+        <div className="main-content"> 
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova Sala</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Nome da Sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente? <a href="#">clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  )
+}
