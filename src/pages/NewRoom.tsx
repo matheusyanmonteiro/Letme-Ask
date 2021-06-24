811c49de924a85e2100caacf6ed7531fde649c3e
v <matheusyanmonteiro@gmail.com>
2021-06-24 10:59:17 -0300

    created all method of auth

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,7 +1,7 @@
-import { useContext } from 'react';
+//import { useContext } from 'react';
 import {Link} from 'react-router-dom';
 
-import { AuthContext } from '../App';
+//import { AuthContext } from '../contexts/AuthContext';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -13,7 +13,7 @@ import { Button } from '../components/Button';
 import '../styles/auth.scss';
 
 export function NewRoom() {
-  const { user } = useContext(AuthContext)
+  //const { user } = useContext(AuthContext)
 
   return (
     <div id="page-auth">
@@ -26,7 +26,6 @@ export function NewRoom() {
       <main>
         <div className="main-content"> 
           <img src={logoImg} alt="Letmeask" />
-          <h1>{user?.name}</h1>
           <h2>Criar uma nova Sala</h2>
           <form>
             <input
